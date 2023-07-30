29320cd1570c0990c1f3f981e71e609694a9c4b7
sanjay <127958856+karan-mango@users.noreply.github.com>
2023-07-30 13:51:01 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,38 @@
+const offscreenmenu=document.querySelector('.off-screen-menu');
+const hamMenu=document.querySelector('.hamburgur-menu');
+
+hamMenu.addEventListener('click', ()=>{
+    hamMenu.classList.toggle('active');
+    offscreenmenu.classList.toggle('active');
+})
+
+function redirectToPage1() {
+    window.location.href = "html2.html";
+  }
+
+  function redirectToPage2() {
+    window.location.href = "html2.html";
+  }
+  function redirectToPage3() {
+    window.location.href = "html2.html";
+  }
+  function redirectToPage4() {
+    window.location.href = "html2.html";
+  }
+  function redirectToPage5() {
+    window.location.href = "html2.html";
+  }
+  function redirectToPage6() {
+    window.location.href = "html2.html";
+  }
+  function redirectToPage7() {
+    window.location.href = "html2.html";
+  }
+  function redirectToPage8() {
+    window.location.href = "html2.html";
+  }
+
+
+
+
+//   after this u have to add the links similarly to all divand departments
---
+++ app3.js
@@ -0,0 +1,15 @@
+const offscreenmenu=document.querySelector('.off-screen-menu');
+const hamMenu=document.querySelector('.hamburgur-menu');
+
+hamMenu.addEventListener('click', ()=>{
+    hamMenu.classList.toggle('active');
+    offscreenmenu.classList.toggle('active');
+})
+
+
+
+function redirectToPage1() {
+    window.location.href = "html4.html";
+  }
+
+
